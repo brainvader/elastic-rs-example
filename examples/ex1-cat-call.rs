50f3068dce68f26dba94b8ff41214adb5a42c40f
BrainVader <brainvade@gmail.com>
2020-05-17 21:04:51 +0900

    Add logic to get es node information

--- examples/ex1-cat-call.rs
+++ examples/ex1-cat-call.rs
@@ -1,8 +1,30 @@
+extern crate elasticsearch_rs_examples as MY;
+
 use elasticsearch::{http::transport::Transport, Elasticsearch};
+use MY::ESNode;
+
+/*
+// Reference
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/cat.html#headers
+*/
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let transport = Transport::single_node("http://127.0.0.1:9200")?;
     let client = Elasticsearch::new(transport);
+    let cat_client = client.cat();
+    let nodes = cat_client
+        .nodes()
+        .h(&["ip", "port", "heapPercent", "name"])
+        .format("json");
+    let response = nodes.send().await?;
+
+    // let response_body = response.text().await?;
+    // dbg!(response_body);
+
+    let response_body: Vec<ESNode> = response.json::<Vec<ESNode>>().await?;
+    if let Some(es_node) = response_body.iter().take(1).next() {
+        println!("{:?}", es_node);
+    }
     Ok(())
 }
