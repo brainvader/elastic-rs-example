f92e649bb7dfafed71b3e8e077121be14e84e8d5
BrainVader <brainvade@gmail.com>
2020-05-17 21:04:02 +0900

    Add es node information struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use serde::Deserialize;
+
+// Cluster node information
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/cat-nodes.html
+#[derive(Deserialize, Debug)]
+pub struct ESNode {
+    ip: String,
+    port: String,
+    #[serde(alias = "heapPercent")]
+    heap_percent: String,
+    name: String,
 }
