b40321bea0139cf96b67a47839286c42d6b836aa
BrainVader <brainvade@gmail.com>
2020-05-18 21:28:09 +0900

    Extract IndexResponseBody out into index module

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use serde::Deserialize;
 
+pub mod api;
+
 // Cluster node information
 // https://www.elastic.co/guide/en/elasticsearch/reference/current/cat-nodes.html
 #[derive(Deserialize, Debug)]
@@ -9,31 +11,4 @@ pub struct ESNode {
     #[serde(alias = "heapPercent")]
     heap_percent: String,
     name: String,
-}
-
-// https://www.elastic.co/guide/en/elasticsearch/reference/7.7/docs-index_.html#docs-index-api-response-body
-#[derive(Deserialize, Debug)]
-struct IndexResponseShard {
-    failed: i64,
-    successful: i64,
-    total: i64,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct IndexResponseBody {
-    #[serde(alias = "_index")]
-    index: String,
-    #[serde(alias = "_type")]
-    r#type: String,
-    #[serde(alias = "_id")]
-    id: String,
-    #[serde(alias = "_version")]
-    version: i64,
-    result: String,
-    #[serde(alias = "_shards")]
-    shards: IndexResponseShard,
-    #[serde(alias = "_seq_no")]
-    seq_no: i64,
-    #[serde(alias = "_primary_term")]
-    primary_term: i64,
 }
