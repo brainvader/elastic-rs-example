872b298eae4ae7da193f51e6b2ee214b1d2cf92b
BrainVader <brainvade@gmail.com>
2020-05-24 12:25:00 +0900

    Add logger setup functin

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use serde::Deserialize;
 
 pub mod api;
 
+use std::io::Write;
+
 // Cluster node information
 // https://www.elastic.co/guide/en/elasticsearch/reference/current/cat-nodes.html
 #[derive(Deserialize, Debug)]
@@ -11,4 +13,27 @@ pub struct ESNode {
     #[serde(alias = "heapPercent")]
     heap_percent: String,
     name: String,
+}
+
+pub const RUST_LOG: &'static str = "RUST_LOG";
+pub const RUST_LOG_STYLE: &'static str = "RUST_LOG_STYLE";
+
+pub fn setup_logger() -> dotenv::Result<()> {
+    let log_level: String = dotenv::var(RUST_LOG)?;
+    let log_style: String = dotenv::var(RUST_LOG_STYLE)?;
+    std::env::set_var(RUST_LOG, log_level);
+    std::env::set_var(RUST_LOG_STYLE, log_style);
+    env_logger::builder()
+        .target(env_logger::Target::Stdout)
+        .format_indent(Some(4))
+        .format(
+            |buf: &mut env_logger::fmt::Formatter, record: &log::Record| {
+                let ts = buf.timestamp();
+                writeln!(buf, "[{}] {}", ts, record.level())?;
+                writeln!(buf, "{}", record.args())
+            },
+        )
+        // .write_style(env_logger::WriteStyle::Auto)
+        .init();
+    Ok(())
 }
